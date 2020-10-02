0fb511aea883e2e84d39379f1fc97eddbf94ab87
Mariya <63921256+MariyaSin@users.noreply.github.com>
2020-10-03 00:27:39 +0300

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+  if ((array == 0) || (array.length == 0))
+    return 0;
+
+  return array.reduce(function(minimal, current){
+    return current < minimal ? current: minimal;
+  }, Infinity);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+  if ((array == 0) || (array.length == 0))
+    return 0;
+  
+  return array.reduce(function(maximal, current){
+    return current > maximal ? current: maximal;
+  }, '');
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 0) {
+  if ((array == 0) || (array.length == 0))
+    return 0;
+
+  return array.reduce(function(s, current){
+    return +s + +current;
+  }, '') / array.length;
 }
